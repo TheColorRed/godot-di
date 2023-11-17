922597b48061fbd89d03a2ea7a9eff08d2087f38
Ryan Naddy <untuned20@gmail.com>
2023-11-17 16:49:23 -0600

    Updates

--- services/game-ref.service.gd
+++ services/game-ref.service.gd
@@ -3,5 +3,11 @@ class_name GameRef extends Injectable
 func _init():
 	provided_in = 'root'
 
+## Gets the game's window.
 var root: Window:
 	get: return Engine.get_main_loop().get_root()
+
+
+## Gets the game's scene tree.
+var tree: SceneTree:
+	get: return root.get_tree()
