97381ef4fd429e52151a5a99fc900a3eab468ad5
Ryan Naddy <untuned20@gmail.com>
2024-07-22 10:22:24 -0500

    Update node ref service

--- services/node-ref.service.gd
+++ services/node-ref.service.gd
@@ -1,8 +1,8 @@
 ## A reference to the current node.
 class_name NodeRef extends Injectable
 
-var node: Variant
+var node: Node
 
-func _init(p_node: Variant=null) -> void:
+func _init(p_node: Node=null) -> void:
 	if p_node != null and p_node is Node:
 		node = p_node
